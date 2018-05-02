0d689ce7abc3dfb7e5fa182c3918aab9d44c6e23
devalstudio devalstudio <devalstudio87@gmail.com>
2018-05-02 20:42:11 +0300

    111

--- resources/assets/js/admin/category-edit.js
+++ resources/assets/js/admin/category-edit.js
@@ -11,6 +11,7 @@ window.categoryEdit = new Vue({
     },
     mounted: function () {
         this.editor = CKEDITOR.replace('category-content');
+     
     },
     methods: {
         start() {
@@ -18,19 +19,19 @@ window.categoryEdit = new Vue({
         },
         saveCategory(){
             this.categoryContent = this.editor.getData();         
-
             let category = {
                 'categoryParent':  this.categoryParent,
                 'categoryTitle': this.categoryTitle,
                 'categoryDescription':  this.categoryDescription,
                 'categoryContent':  this.categoryContent       
             };
-
             axios.post('/admin/categories', {
                 category: category
             }).then(function (response) {
-                
+                location.href = "/admin/categories";
             });
         }
     }
     });     
+
+    
